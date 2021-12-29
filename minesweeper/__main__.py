25e98afdf7a6ee9365d3d4afde2422c333879281
jgesc <37676259+jgesc@users.noreply.github.com>
2021-12-29 19:18:06 +0100

    Parse arguments

--- minesweeper/__main__.py
+++ minesweeper/__main__.py
@@ -1,5 +1,18 @@
-from http.server import ThreadingHTTPServer
+from http.server import ThreadingHTTPServer, HTTPServer
 from handler import MinesweeperRequestHandler
+import argparse
 
-httpd = ThreadingHTTPServer(('', 8000), MinesweeperRequestHandler)
+# Arguments
+parser = argparse.ArgumentParser(description='Minesweeper HTTP API')
+parser.add_argument('--no-threading', help='do not enable multithreading',
+    action='store_true')
+parser.add_argument('-p', '--port', type=int, help="port to listen on",
+    default=8080)
+parser.add_argument('-i', '--ip', type=str, help="host IP to listen on",
+    default='')
+args = parser.parse_args()
+
+# Start server
+server_class = HTTPServer if args.no_threading else ThreadingHTTPServer
+httpd = server_class((args.ip, args.port), MinesweeperRequestHandler)
 httpd.serve_forever()
